51a89eb343877825ad0aa73eba8f9a0efff80ac3
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-27 15:30:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,7 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    binding.pry
+    #binding.pry
     result = gets.strip
 
     case result
